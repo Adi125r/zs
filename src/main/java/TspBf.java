e353d9e82b9360671fdf7bf000b308d1cc2368b8
Adrian <awojcieszek@oakfusion.pl>
2019-12-12 08:26:34 +0100

    fix permutation

--- src/main/java/TspBf.java
+++ src/main/java/TspBf.java
@@ -3,14 +3,13 @@ import org.apache.commons.collections4.iterators.PermutationIterator;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class TspBf
-{
+public class TspBf {
     private static TspReader tspReader;
-    private static int numberthread=6;
-    private static int temp=1;
+    private static int numberthread = 12;
+    private static int temp = 1;
     private static int numpoints;
-    private static int numFactorial ;
-    private static Map<Integer,List<Integer>> theBest = new HashMap<Integer, List<Integer>>();
+    private static int numFactorial;
+    private static Map<Integer, List<Integer>> theBest = new HashMap<Integer, List<Integer>>();
     private static PermutationIterator permutationGenerator;
 
     private static void generateData() {
@@ -25,13 +24,15 @@ public class TspBf
 
     private static int factorial(int num) {
         int result = 1;
-        for (int i = 1; i < (num-1); i++) {
+        for (int i = 1; i < (num - 1); i++) {
             result *= i;
         }
         System.out.println(result);
         return result;
     }
-
+private synchronized  static List<Integer> getPermutation(){
+        return permutationGenerator.next();
+}
 
     private static void algorithmBf() throws InterruptedException {
 
@@ -39,13 +40,11 @@ public class TspBf
             @Override
             public void run() {
                 List<Integer> possibleRoad;
-                int theBestRoadValue=Integer.MAX_VALUE;
-                for (int i = numpoints *(temp-1); i < numpoints*temp; i++)
-                {
+                int theBestRoadValue = Integer.MAX_VALUE;
+                for (int i = numpoints * (temp - 1); i < numpoints * temp; i++) {
 
-
-                    possibleRoad = permutationGenerator.next();
-                    int temporaryRoadValue =0;
+                    possibleRoad = getPermutation();
+                    int temporaryRoadValue = 0;
                     temporaryRoadValue = temporaryRoadValue + tspReader.getGraph()[0][possibleRoad.get(0)];
                     for (int j = 0; j < possibleRoad.size() - 1; j++) {
                         temporaryRoadValue = temporaryRoadValue + tspReader.getGraph()[possibleRoad.get(j)][possibleRoad.get(j + 1)];
@@ -56,53 +55,48 @@ public class TspBf
                         theBestRoadValue = temporaryRoadValue;
                         synchronized (this) {
                             theBest.put(temporaryRoadValue, possibleRoad);
-
                         }
                     }
-
-                }}
-
+                }
+            }
         };
         Thread[] threadBf = new Thread[numberthread];
-        for (int i=0;i<numberthread;i++){
+        for (int i = 0; i < numberthread; i++) {
             Thread thread = new Thread(runnableBf);
-            threadBf[i]=thread;
+            threadBf[i] = thread;
         }
 
 
-for (int i =0;i<numberthread;i++) {
-    numpoints =( numFactorial/ numberthread);
-    threadBf[i].start();
-    temp++;
+        for (int i = 0; i < numberthread; i++) {
+            numpoints = (numFactorial / numberthread);
+            threadBf[i].start();
+            temp++;
 
-}
-        for (int i =0;i<numberthread;i++) {
-          threadBf[i].join();}
-temp=1;
+        }
+        for (int i = 0; i < numberthread; i++) {
+            threadBf[i].join();
+        }
+        temp = 1;
     }
 
-    private static void printScore()
-    {
+    private static void printScore() {
         Map<Integer, List<Integer>> sort = new TreeMap<Integer, List<Integer>>(theBest);
-        Map.Entry<Integer,List<Integer>> entry =sort.entrySet().iterator().next();
+        Map.Entry<Integer, List<Integer>> entry = sort.entrySet().iterator().next();
         System.out.println(entry.getKey());
         System.out.print("0 ");
-        for (int i = 0; i < sort.get(entry.getKey()).size(); i++)
-        {
+        for (int i = 0; i < sort.get(entry.getKey()).size(); i++) {
 
-            System.out.print(entry.getValue().get(i)+" ");
+            System.out.print(entry.getValue().get(i) + " ");
         }
         System.out.print("0\n");
     }
 
 
-
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        tspReader = new TspReader("14.txt");
-       // tspReader.writeGraph();
+        tspReader = new TspReader("10.txt");
+        // tspReader.writeGraph();
         generateData();
-        for (int i = 0; i < 1; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             long firstTime = System.nanoTime();
             algorithmBf();
             long secondTime = System.nanoTime();
